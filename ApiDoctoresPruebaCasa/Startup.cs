659703f8d06bb5d0f11f1f709c9a8ca6c5e8b905
Alexandru Preutesei <alexandru.preutesei@tajamar365.com>
2022-05-02 18:26:37 +0200

    commit

--- ApiDoctoresPruebaCasa/Startup.cs
+++ ApiDoctoresPruebaCasa/Startup.cs
@@ -29,6 +29,7 @@ namespace ApiDoctoresPruebaCasa
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("AllowOrigin", x => x.AllowAnyOrigin()));
             string url = this.Configuration.GetConnectionString("CadenaAWS");
             services.AddTransient<RepositoryDoctores>();
             services.AddDbContext<DoctoresContext>(
@@ -47,7 +48,7 @@ namespace ApiDoctoresPruebaCasa
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(options => options.AllowAnyOrigin());
             app.UseSwagger();
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiDoctoresPruebaCasa v1");
